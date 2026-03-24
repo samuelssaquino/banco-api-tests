935fd3ce2c0b1f512b14e53629473754a8b031aa
Samuel Aquino <samuelssaquino@outlook.com>
2026-03-24 20:37:57 +0000

    Iniciando o projeto

---
+++ test/login.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Login', () => {
+    describe('POST /Login', () => {
+        it('Deve retornar 200 com token em string quando usart credencias válidas', async() => {
+            const resposta = await request('http://localhost:3000')
+                .post('/Login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                })            
+
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body.token).to.be.a('string');
+        })
+    })
+})
